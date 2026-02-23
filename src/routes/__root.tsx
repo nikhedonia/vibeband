d45a799d5b42a20bd8f552d11aa18aa5cc639be1
nikhedonia <Gaetano.Checinski@gmail.com>
2026-02-23 09:03:45 +0000

    initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,69 @@
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+
+import WorkOSProvider from '../integrations/workos/provider'
+
+import appCss from '../styles.css?url'
+
+import type { ApolloClientIntegration } from '@apollo/client-integration-tanstack-start'
+
+interface MyRouterContext extends ApolloClientIntegration.RouterContext {}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  shellComponent: RootDocument,
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <WorkOSProvider>
+          <Header />
+          {children}
+          <TanStackDevtools
+            config={{
+              position: 'bottom-right',
+            }}
+            plugins={[
+              {
+                name: 'Tanstack Router',
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+            ]}
+          />
+        </WorkOSProvider>
+        <Scripts />
+      </body>
+    </html>
+  )
+}
